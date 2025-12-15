381e5498943b8ae46b616afe4a775e5685b409aa
shubham-Alhat <shubhamalhat520@gmail.com>
2025-12-15 20:49:36 +0530

    add cors and cookie-parser middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.get("/demo", async (req, res) => {
-  res.send("shubham");
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get("/api/v1", (req, res) => {
+  res.send("welcome to backend...");
 });
 
 export default app;
